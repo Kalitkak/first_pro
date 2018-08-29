ef5024b6c0c0a34ef11b67c6b9fe16080bcad9f6
kalitkak <kalitka.kse@gmail.com>
2018-08-29 22:54:47 +0200

    change title

--- db/migrate/20180827084723_create_products.rb
+++ db/migrate/20180827084723_create_products.rb
@@ -1,10 +1,9 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
-      t.string :name
-      t.text :descriotion
-      t.string :image
-      t.string :url
+      t.string :title
+      t.text :description
+      t.string :image_url
 
       t.timestamps
     end
