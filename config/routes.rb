b29f0170b423eb55d0f9be75a5b5b13d53304556
kalitkak <kalitka.kse@gmail.com>
2018-08-29 21:14:21 +0200

    chages

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :products
   get 'simple_pages/about'
   get 'simple_pages/contact'
-root 'simple_pages#index'
+  root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
