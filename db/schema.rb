128e64467c0154aaf4e90088a930846d8eaa3dc0
kalitkak <kalitka.kse@gmail.com>
2018-08-29 22:50:09 +0200

    change title

--- db/schema.rb
+++ db/schema.rb
@@ -14,9 +14,8 @@ ActiveRecord::Schema.define(version: 2018_08_27_084723) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.text "descriotion"
-    t.string "image"
-    t.string "url"
+    t.text "description"
+    t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
